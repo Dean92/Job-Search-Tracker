fbe1ee863ab016c8b2228a81c6d079ac1d4272cc
Dean McCoy <dean92@gmail.com>
2023-03-23 16:37:30 -0500

    Created account service. Refactored nav bar. Added ngx bootstrap for dropdown.

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -7,10 +7,23 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
-  declarations: [AppComponent, NavMenuComponent, HomeComponent],
-  imports: [BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }), HttpClientModule, FormsModule, RouterModule.forRoot([{ path: '', component: HomeComponent, pathMatch: 'full' }])],
+  declarations: [AppComponent, NavMenuComponent, HomeComponent, LoginComponent],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    BsDropdownModule.forRoot(),
+    RouterModule.forRoot([
+      // { path: 'login', component: LoginComponent },
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,16 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.css'],
 })
-export class NavMenuComponent {
-  // isExpanded = false;
-  // collapse() {
-  //   this.isExpanded = false;
-  // }
-  // toggle() {
-  //   this.isExpanded = !this.isExpanded;
-  // }
+export class NavMenuComponent implements OnInit {
+  model: any = {};
+  loggedIn = false;
+
+  constructor(private accountService: AccountService) {}
+
+  ngOnInit(): void {
+    this.getCurrentUser();
+  }
+
+  getCurrentUser() {
+    this.accountService.currenUser$.subscribe({
+      next: (user) => (this.loggedIn = !!user),
+      error: (error) => console.log(error),
+    });
+  }
+
+  login() {
+    this.accountService.login(this.model).subscribe({
+      next: (response) => {
+        console.log(response);
+        this.loggedIn = true;
+      },
+      error: (error) => console.log(error),
+    });
+  }
+
+  logout() {
+    this.accountService.logout();
+    this.loggedIn = false;
+  }
 }
